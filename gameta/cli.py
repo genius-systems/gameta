9450b3898446d620121576befc93460ffdc2797d
David Lee <ltw_david@hotmail.com>
2020-11-10 14:20:37 +0800

    Updating and printing version of Gameta, adding unit tests for this

--- gameta/cli.py
+++ gameta/cli.py
@@ -5,6 +5,7 @@ from os.path import abspath
 import click
 
 from .context import gameta_context, GametaContext
+from gameta import __version__
 
 
 __all__ = [
@@ -13,24 +14,30 @@ __all__ = [
 ]
 
 
-@click.group('gameta')
+@click.group('gameta', invoke_without_command=True)
 @click.option('--project-dir', '-d', type=str, default=getcwd())
+@click.option('--version', '-v', is_flag=True, default=False)
 @gameta_context
-def gameta_cli(context: GametaContext, project_dir: str) -> None:
+def gameta_cli(context: GametaContext, project_dir: str, version: bool) -> None:
     """
     Gameta CLI, contains commands to simplify your DevOps operations
     \f
     Args:
         context (GametaContext): Gameta Context
         project_dir (str): Project directory, defaults to the current working directory
+        version (bool): Flag to print Gameta's version and exit
 
     Returns:
         None
 
     Examples:
-        $ gameta  # Use all default values
+        $ gameta -v  # Prints version and exits
         $ gameta -d /path/to/project/dir  # Specify a project directory
     """
+    if version:
+        click.echo(f"Gameta version: {__version__}")
+        return
+
     context.project_dir = abspath(project_dir)
     chdir(context.project_dir)
 
---
+++ tests/test_cli.py
@@ -0,0 +1,30 @@
+
+import zipfile
+from os.path import join, dirname
+from unittest import TestCase
+from unittest.mock import patch
+
+from click.testing import CliRunner
+
+from gameta.context import GametaContext
+from gameta.cli import gameta_cli
+from gameta import __version__
+
+
+class TestGametaCli(TestCase):
+    def setUp(self) -> None:
+        self.runner = CliRunner()
+        self.cli = gameta_cli
+
+    @patch('gameta.cli.click.Context.ensure_object')
+    def test_cli_printing_version(self, mock_ensure_object):
+        with self.runner.isolated_filesystem() as f:
+            with zipfile.ZipFile(join(dirname(__file__), 'data', 'git.zip'), 'r') as template:
+                template.extractall(f)
+            context = GametaContext()
+            context.project_dir = f
+            context.load()
+            mock_ensure_object.return_value = context
+            result = self.runner.invoke(self.cli, ["-v"])
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(result.output, f"Gameta version: {__version__}\n")
