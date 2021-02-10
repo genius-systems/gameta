9dac2ad2312de44294148cac7ba46e704edd9ae2
David Lee <ltw_david@hotmail.com>
2021-02-10 14:37:56 +0800

    Splitting init and sync into separate commands

--- gameta/init.py
+++ gameta/init.py
@@ -3,16 +3,15 @@ from typing import Optional
 
 import click
 
-from git import Repo, InvalidGitRepositoryError, GitError
+from git import Repo, InvalidGitRepositoryError
 
-from .cli import gameta_cli
 from gameta.base import gameta_context, GametaContext
 
 
-__all__ = ['init', 'sync']
+__all__ = ['init']
 
 
-@gameta_cli.command()
+@click.command()
 @click.option('--git', '-g', is_flag=True, default=False, help='Flag to initialise the current directory as a git repo')
 @click.option('--overwrite', '-o', is_flag=True, default=False, help='Flag to overwrite existing .gameta file')
 @gameta_context
@@ -65,42 +64,5 @@ def init(context: GametaContext, overwrite: bool, git: bool) -> None:
         }
         context.export()
         click.echo(f"Successfully initialised {name} as a metarepo")
-    except Exception as e:
-        raise click.ClickException(f"{e.__class__.__name__}.{str(e)}")
-
-
-@gameta_cli.command()
-@gameta_context
-def sync(context: GametaContext) -> None:
-    """
-    Syncs all the repositories listed in the .gameta file locally
-    \f
-    Args:
-        context (GametaContext): Gameta Context
-
-    Returns:
-        None
-
-    Examples:
-        $ gameta sync
-
-    Raises:
-        click.ClickException: If errors occur during processing
-    """
-    click.echo(f"Syncing all child repositories in metarepo {context.project_dir}")
-
-    try:
-        for repo, details in context.repositories.items():
-
-            # We assume the metarepo has already been cloned
-            if context.is_primary_metarepo(repo):
-                continue
-
-            try:
-                Repo.clone_from(details['url'], details['path'])
-                click.echo(f'Successfully synced {repo} to {details["path"]}')
-            except GitError as e:
-                click.echo(f"An error occurred {str(e)}, skipping repo")
-                continue
     except Exception as e:
         raise click.ClickException(f"{e.__class__.__name__}.{str(e)}")
---
+++ tests/test_sync.py
@@ -0,0 +1,93 @@
+import json
+import zipfile
+from os import listdir
+from os.path import join, dirname
+from shutil import copyfile
+
+from unittest import TestCase
+from unittest.mock import patch
+
+from click.testing import CliRunner
+
+from gameta.base import GametaContext
+from gameta.sync import sync
+
+
+class TestSync(TestCase):
+    def setUp(self) -> None:
+        self.runner = CliRunner()
+        self.sync = sync
+
+    @patch('gameta.cli.click.Context.ensure_object')
+    def test_sync_empty_meta_file(self, mock_ensure_object):
+        with self.runner.isolated_filesystem() as f:
+            with zipfile.ZipFile(join(dirname(__file__), 'data', 'git.zip'), 'r') as template:
+                template.extractall(f)
+            with open(join(f, '.gameta'), 'w') as m:
+                json.dump(
+                    {
+                        'repositories': {},
+                        "commands": {}
+                    }, m
+                )
+            context = GametaContext()
+            context.project_dir = f
+            context.load()
+            mock_ensure_object.return_value = context
+            result = self.runner.invoke(self.sync)
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(
+                result.output,
+                f'Syncing all child repositories in metarepo {f}\n'
+            )
+
+    @patch('gameta.cli.click.Context.ensure_object')
+    def test_sync_all_repos(self, mock_ensure_object):
+        with self.runner.isolated_filesystem() as f:
+            with zipfile.ZipFile(join(dirname(__file__), 'data', 'git.zip'), 'r') as template:
+                template.extractall(f)
+            copyfile(join(dirname(__file__), 'data', '.gameta_other_repos'), join(f, '.gameta'))
+            context = GametaContext()
+            context.project_dir = f
+            context.load()
+            mock_ensure_object.return_value = context
+            result = self.runner.invoke(self.sync)
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(
+                result.output,
+                f'Syncing all child repositories in metarepo {f}\n'
+                'Successfully synced GitPython to GitPython\n'
+                'Successfully synced gitdb to core/gitdb\n'
+            )
+            self.assertCountEqual(listdir(f), ['GitPython', 'core', '.git', '.gameta'])
+            self.assertTrue(all(i in listdir(join(f, 'GitPython')) for i in ['git', 'doc', 'test']))
+            self.assertCountEqual(listdir(join(f, 'core')), ['gitdb'])
+            self.assertTrue(all(i in listdir(join(f, 'core', 'gitdb')) for i in ['gitdb', 'doc', 'setup.py']))
+
+    @patch('gameta.cli.click.Context.ensure_object')
+    def test_sync_repo_already_exists(self, mock_ensure_object):
+        with self.runner.isolated_filesystem() as f:
+            with zipfile.ZipFile(join(dirname(__file__), 'data', 'git.zip'), 'r') as template:
+                template.extractall(f)
+            with zipfile.ZipFile(join(dirname(__file__), 'data', 'gitdb.zip'), 'r') as template:
+                template.extractall(join(f, 'core'))
+            copyfile(join(dirname(__file__), 'data', '.gameta_other_repos'), join(f, '.gameta'))
+            context = GametaContext()
+            context.project_dir = f
+            context.load()
+            mock_ensure_object.return_value = context
+            result = self.runner.invoke(self.sync)
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(
+                result.output,
+                f'Syncing all child repositories in metarepo {f}\n'
+                'Successfully synced GitPython to GitPython\n'
+                "An error occurred Cmd('git') failed due to: exit code(128)\n"
+                "  cmdline: git clone -v https://github.com/gitpython-developers/gitdb.git core/gitdb\n"
+                "  stderr: 'fatal: destination path 'core/gitdb' already exists and is not an empty directory.\n"
+                "', skipping repo\n"
+            )
+            self.assertCountEqual(listdir(f), ['GitPython', 'core', '.git', '.gameta'])
+            self.assertTrue(all(i in listdir(join(f, 'GitPython')) for i in ['git', 'doc', 'test']))
+            self.assertCountEqual(listdir(join(f, 'core')), ['gitdb'])
+            self.assertTrue(all(i in listdir(join(f, 'core', 'gitdb')) for i in ['gitdb', 'doc', 'setup.py']))
