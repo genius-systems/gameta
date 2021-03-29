240955318b058a06947bd942807b755b4ec201cc
David Lee <ltw_david@hotmail.com>
2021-03-29 23:15:24 +0800

    Adding new Gameta Files interface

---
+++ gameta/base/files.py
@@ -0,0 +1,70 @@
+from abc import abstractmethod
+from os.path import join, exists
+from typing import Any
+
+
+__all__ = ['File']
+
+
+class File(object):
+    """
+    Generic file interface for Gameta files
+
+    Attributes:
+        path (str): Absolute path to the folder containing the file
+        file_name (str): Name of the reference file
+    """
+
+    def __init__(self, path: str, file_name: str):
+        self.path = path
+        self.file_name = file_name
+
+    @property
+    def file(self) -> str:
+        """
+        Returns the absolute path to the reference file
+
+        Returns:
+            str: Absolute path to the file
+        """
+        return join(self.path, self.file_name)
+
+    def exists(self) -> bool:
+        """
+        Returns a boolean indicating if the file exists
+
+        Returns:
+            bool: Boolean indicator if the file exists
+        """
+        return exists(self.file)
+
+    def create(self) -> None:
+        """
+        Creates an empty file
+
+        Returns:
+            None
+        """
+        with open(self.file, 'w+'):
+            pass
+
+    @abstractmethod
+    def load(self) -> Any:
+        """
+        Abstractmethod to load and return data from the file
+
+        Returns:
+            Any
+        """
+
+    @abstractmethod
+    def export(self, data: Any) -> None:
+        """
+        Abstractmethod to export data to the file
+
+        Args:
+            data (Any): Data to be exported
+
+        Returns:
+            None
+        """
