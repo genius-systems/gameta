b38a8781819d5862978c6dddebfbb3854a3d37b6
David Lee <ltw_david@hotmail.com>
2021-04-02 03:22:33 +0800

    Exposing new functionality in base.__init__.py

--- gameta/base/__init__.py
+++ gameta/base/__init__.py
@@ -2,13 +2,17 @@
 from .files import File
 from .schemas import supported_versions, Schema, to_schema_tuple, to_schema_str
 from .vcs import vcs_interfaces, VCS, GametaRepo
-from .command import Command
-from .context import GametaContext, gameta_context
+from .parameters import Parameter
+from .commands import Runner
+from .contexts import GametaContext, gameta_context
 
 
 __all__ = [
     # Commands
-    'Command',
+    'Runner',
+
+    # Parameters
+    'Parameter',
 
     # Schemas
     'supported_versions', 'Schema', 'to_schema_tuple', 'to_schema_str',
