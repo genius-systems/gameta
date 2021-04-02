9af37f989125b33ebd26b4aec513d0d7af96936b
David Lee <ltw_david@hotmail.com>
2021-04-02 13:21:40 +0800

    Updating tests for main CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,8 @@
 
 import zipfile
+from os import getcwd
 from os.path import join, dirname
+from shutil import copytree
 from unittest import TestCase
 from unittest.mock import patch
 
@@ -19,12 +21,47 @@ class TestGametaCli(TestCase):
         self.cli = gameta_cli
 
     @patch('gameta.cli.click.Context.ensure_object')
+    def test_cli_change_project_folder(self, mock_ensure_object):
+        with self.runner.isolated_filesystem() as f:
+            copytree(join(dirname(__file__), 'data', 'gameta_single_repo_only'), join(f, 'test'))
+            with zipfile.ZipFile(join(dirname(__file__), 'data', 'git.zip'), 'r') as template:
+                template.extractall(join(f, 'test'))
+            context = GametaContext()
+            mock_ensure_object.return_value = context
+            result = self.runner.invoke(self.cli, ["-d", join(f, 'test')])
+            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(getcwd(), join(f, 'test'))
+            self.assertEqual(
+                context.repositories,
+                {
+                    'gameta': {
+                        '__metarepo__': True,
+                        'path': '.',
+                        'tags': ['metarepo'],
+                        'url': 'git@github.com:genius-systems/gameta.git',
+                        'vcs': 'git'
+                    }
+                }
+            )
+
+    @patch('gameta.cli.click.Context.ensure_object')
+    def test_cli_project_folder_does_not_exist(self, mock_ensure_object):
+        with self.runner.isolated_filesystem() as f:
+            context = GametaContext()
+            mock_ensure_object.return_value = context
+            result = self.runner.invoke(self.cli, ["-d", join(f, 'test')])
+            self.assertEqual(result.exit_code, 1)
+            self.assertEqual(
+                result.output,
+                f"Error: Project directory {join(f, 'test')} does not exist\n"
+            )
+
+    @patch('gameta.cli.click.Context.ensure_object')
     def test_cli_printing_version(self, mock_ensure_object):
         with self.runner.isolated_filesystem() as f:
             with zipfile.ZipFile(join(dirname(__file__), 'data', 'git.zip'), 'r') as template:
                 template.extractall(f)
             context = GametaContext()
-            context.init(f)
             mock_ensure_object.return_value = context
             result = self.runner.invoke(self.cli, ["-v"])
             self.assertEqual(result.exit_code, 0)
